31db588914a0f3ef77fd5f1c50c3990c2916729e
Breno <breno.givas@icloud.com>
2023-04-21 17:16:48 -0300

    JS ProjetoEfeito e URL

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Ativar Efeito do Menu
+
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link){
+    const url = location.href;
+    const href = link.href;
+
+    if(url.includes(href)){
+        link.classList.add('ativo')
+    }
+}
+
+links.forEach(ativarLink)
+
+// Ativar Itens do Forms do orcamento 
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true
+        console.log(elemento)
+    }
+}
+
+parametros.forEach(ativarProduto)
